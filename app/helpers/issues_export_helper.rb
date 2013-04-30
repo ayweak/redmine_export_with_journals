fe86d70d241c7b6769da6330d3eb530039986d76
ayweak <ayweak@users.noreply.github.com>
2013-05-01 01:14:33 +0900

    履歴格納を単一セルか複数セルで選択できるようにした

--- app/helpers/issues_export_helper.rb
+++ app/helpers/issues_export_helper.rb
@@ -1,12 +1,25 @@
 module IssuesExportHelper
-  def add_journals(csv)
+  DELIMITER_LINE = '=' * 5
+
+  def add_journals(csv, query, options={})
     csv_with_journals = FCSV.generate do |newcsv|
       FCSV.parse(Redmine::CodesetUtil.to_utf8(csv, 'CP932'), :headers => true, :return_headers => true) do |row|
         if row.header_row?
           newcsv << row.fields + [t(:label_history)]
         else
-          newcsv << row.fields + Issue.find(row.fields.first).journals.map do |j|
-            j.user.name + ' (' + format_time(j.created_on) + ')' + "\n" + j.details.map {|d| show_detail(d, true)}.join("\n") + "\n" + (j.notes.nil? ? '' : j.notes)
+          if options[:single_cell] == 'single'
+            histories = Issue.find(row.fields.first).journals.map do |j|
+              j.user.name + ' (' + format_time(j.created_on) + ')' + "\n" +
+                (j.details.empty? ? "\n" : "\n" + j.details.reduce('') {|s, d| s += '* ' + show_detail(d, true) + "\n"} + "\n") +
+                (j.notes.nil? ? '' : j.notes.gsub(/\r\n?/, "\n"))
+            end
+            newcsv << row.fields + [histories.join("\n" + DELIMITER_LINE + "\n")]
+          else
+            newcsv << row.fields + Issue.find(row.fields.first).journals.map do |j|
+              j.user.name + ' (' + format_time(j.created_on) + ')' + "\n" +
+                j.details.map {|d| show_detail(d, true)}.join("\n") + "\n" +
+                (j.notes.nil? ? '' : j.notes)
+            end
           end
         end
       end
